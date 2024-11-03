15e0fe2c24e9e6e07883364375c67926c16b5289
Jeanettetech <jeanetteintech@gmail.com>
2024-11-03 00:06:36 +0000

    repo update

--- src/test/java/com/codingblackfemales/CalculatorTests.java
+++ src/test/java/com/codingblackfemales/CalculatorTests.java
@@ -18,9 +18,50 @@ public class CalculatorTests {
 
     @Test
     @DisplayName("subtract two numbers")
-    public void testSubtraction() {
+    public void testSubstraction() {
         final Integer difference = Calculator.subtract(2, 4);
 
         assertEquals(-2, difference);
     }
+
+    @Test
+    @DisplayName("multiply two numbers")
+    public void testMultiplication() {
+        final Integer product = Calculator.multiply(3, 3);
+
+        assertEquals(9, product);
+    }
+
+    @Test
+    @DisplayName("divide two numbers")
+    public void testDivision() {
+        final Integer quotient = Calculator.divide(4, 2);
+
+        assertEquals(2, quotient);
+    }
+
+    @Test
+    @DisplayName("multiply a negative and a positive number")
+    public void testMultiplicationWithNegativeAndPositive() {
+        final Integer product = Calculator.multiply(3, -1);
+
+        assertEquals(-3, product);
+    }
+
+    @Test
+    @DisplayName("multiply a number times zero")
+    public void testMultiplicationByZero() {
+        final Integer product = Calculator.multiply(4, 0);
+
+        assertEquals(0, product);
+    }
+
+    @Test
+    @DisplayName("multiply two negative numbers")
+    public void testMultiplicationWithTwoNegatives() {
+        final Integer product = Calculator.multiply(-3, -3);
+
+        assertEquals(9, product);
+    }
+
 }
